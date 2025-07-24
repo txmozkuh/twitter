9e11ab4194ed519cea3e14c634333dbf930e5e64
Daeva <thdang0911@gmail.com>
2025-07-24 15:09:41 +0700

    swagger upt

--- server/src/routes/bookmark.routes.ts
+++ server/src/routes/bookmark.routes.ts
@@ -6,12 +6,59 @@ const bookmarkRouter = Router()
 
 /**
  * @swagger
- * tags:
- *   name: Bookmarks
- *   description: Bookmark tweets endpoint
+ *   /bookmarks:
+ *     post:
+ *       summary: Bookmark a tweet
+ *       description: Allows an authenticated user to bookmark a tweet by its ID.
+ *       tags:
+ *         - Bookmarks
+ *       security:
+ *         - bearerAuth: []
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               required:
+ *                 - tweet_id
+ *               properties:
+ *                 tweet_id:
+ *                   type: string
+ *                   description: ID of the tweet to bookmark
+ *       responses:
+ *         '200':
+ *           description: Successfully bookmark a tweet
+ *         '400':
+ *           description: Failed to request
  */
 
 bookmarkRouter.post('/', accessTokenValidator, createBookmarkValidator, validateRequest, createBookmarkController)
+
+/**
+ * @swagger
+ *   /bookmarks/tweet/{tweet_id}:
+ *     delete:
+ *       summary: Unbookmark a tweet
+ *       description: Allows an authenticated user to unbookmark a tweet by its ID.
+ *       tags:
+ *         - Bookmarks
+ *       security:
+ *         - bearerAuth: []
+ *       parameters:
+ *         - name: tweet_id
+ *           in: path
+ *           required: true
+ *           schema:
+ *             type: string
+ *           description: ID of the tweet to unbookmark
+ *       responses:
+ *         '200':
+ *           description: Successfully unbookmark tweet
+ *         '400':
+ *           description: Failed to request
+ */
+
 bookmarkRouter.delete(
   '/tweet/:tweet_id',
   accessTokenValidator,
--- server/src/routes/like.routes.ts
+++ server/src/routes/like.routes.ts
@@ -9,12 +9,59 @@ const likeRouter = Router()
 
 /**
  * @swagger
- * tags:
- *   name: Like
- *   description: Tweet like endpoints
+ *   /likes:
+ *     post:
+ *       summary: Like a tweet
+ *       description: Allows an authenticated user to like a tweet by its ID.
+ *       tags:
+ *         - Likes
+ *       security:
+ *         - bearerAuth: []
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               required:
+ *                 - tweet_id
+ *               properties:
+ *                 tweet_id:
+ *                   type: string
+ *                   description: ID of the tweet to like
+ *       responses:
+ *         '200':
+ *           description: Successfully like tweet
+ *         '400':
+ *           description: Failed to request
  */
 
 likeRouter.post('/', accessTokenValidator, createLikeValidator, validateRequest, createLikeController)
+
+/**
+ * @swagger
+ *   /likes/tweet/{tweet_id}:
+ *     delete:
+ *       summary: Unlike a tweet
+ *       description: Allows an authenticated user to unlike a tweet by its ID.
+ *       tags:
+ *         - Likes
+ *       security:
+ *         - bearerAuth: []
+ *       parameters:
+ *         - name: tweet_id
+ *           in: path
+ *           required: true
+ *           schema:
+ *             type: string
+ *           description: ID of the tweet to unlike
+ *       responses:
+ *         '200':
+ *           description: Successfully unlike tweet
+ *         '400':
+ *           description: Failed to request
+ */
+
 likeRouter.delete('/tweet/:tweet_id', accessTokenValidator, deleteLikeValidator, validateRequest, deleteLikeController)
 
 export default likeRouter
